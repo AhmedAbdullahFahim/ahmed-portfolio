e720bf93b32ca03402678d5a59b9c43ec746bcbc
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-03-21 22:38:34 +0200

    only removed node modules from sanity

---
+++ portfolio-sanity/schemas/expertise.ts
@@ -0,0 +1,27 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'expertise',
+  title: 'Expertise',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'expertiseTitle',
+      title: 'ExpertiseTitle',
+      type: 'string',
+    }),
+    defineField({
+      name: 'expertiseImage',
+      title: 'ExpertiseImage',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'expertiseInfo',
+      title: 'ExpertiseInfo',
+      type: 'string',
+    }),
+  ],
+})
---
+++ portfolio-sanity/schemas/project.ts
@@ -0,0 +1,61 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      description: 'Title of the project',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'summary',
+      title: 'Summary',
+      type: 'text',
+    }),
+    // next field is to use when the previous one doesn't work properly
+    // defineField({
+    //   name: 'summary',
+    //   title: 'Summary',
+    //   type: 'string',
+    // }),
+    defineField({
+      name: 'technologiesMobile',
+      title: 'TechnologiesMobile',
+      type: 'string',
+    }),
+    defineField({
+      name: 'order',
+      title: 'Order',
+      type: 'number',
+    }),
+    // next field is to use when the previous one doesn't work properly
+    defineField({
+      name: 'technologies',
+      title: 'Technologies',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+    defineField({
+      name: 'linkToBuild',
+      title: 'LinkToBuild',
+      type: 'url',
+    }),
+    defineField({
+      name: 'linkToCode',
+      title: 'LinkToCode',
+      type: 'url',
+    }),
+  ],
+})
