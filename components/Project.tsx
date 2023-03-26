678d538456348aac32f99d4d1592a0deed3c6fa5
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-03-26 04:47:45 +0200

    project height

--- components/Project.tsx
+++ components/Project.tsx
@@ -22,7 +22,7 @@ const Project = ({ project }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='w-screen h-[94vh] sm:h-screen flex flex-col snap-center flex-shrink-0 justify-center items-center px-0 space-y-0 relative'
+      className='w-screen h-screen flex flex-col snap-center flex-shrink-0 justify-center items-center px-0 space-y-0 relative'
     >
       <motion.div
         initial={{ opacity: 0, y: -200 }}
