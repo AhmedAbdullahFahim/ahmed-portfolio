5fb08df0c26206b86dc37ab09501b4295f3509ef
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-03-26 04:49:27 +0200

    project screen height

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const Projects = ({}: Props) => {
   return (
-    <div className='h-[90vh] sm:h-screen flex items-center justify-evenly relative max-w-full overflow-hidden flex-col md:flex-row text-left mx-auto z-0'>
+    <div className='h-[93vh] sm:h-screen flex items-center justify-evenly relative max-w-full overflow-hidden flex-col md:flex-row text-left mx-auto z-0'>
       <h3 className='title top-20 '>Projects</h3>
       <div className='relative w-full flex overflow-x-scroll snap-mandatory overflow-y-hidden snap-x z-20 scrollbar-track-gray-400/20 scrollbar-thumb-gray-400 scrollbar-thin'>
         {projects.map((project, index) => (
